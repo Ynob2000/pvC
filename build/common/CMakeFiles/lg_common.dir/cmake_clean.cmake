44d06c8353e80b8dbc44688908fe9cbd3ef740e9
nlepleux <nlepleux@gmail.com>
2020-07-12 05:28:06 +0200

    fix object names#

---
+++ build/common/CMakeFiles/lg_common.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lg_common.dir/src/framebuffer.c.o"
+  "CMakeFiles/lg_common.dir/src/ivshmem_com.cpp.o"
+  "CMakeFiles/lg_common.dir/src/option.c.o"
+  "CMakeFiles/lg_common.dir/src/stringlist.c.o"
+  "CMakeFiles/lg_common.dir/src/stringutils.c.o"
+  "liblg_common.a"
+  "liblg_common.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/lg_common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/games/CMakeFiles/games.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/games.dir/Includes.cpp.o"
+  "CMakeFiles/games.dir/dayz/dayzread.cpp.o"
+  "CMakeFiles/games.dir/tarkov/tarkovread.cpp.o"
+  "libgames.a"
+  "libgames.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/games.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
